d8b1a616ab85a08d3a278e69b7dd40464f082f71
Tuxliri <dvde.iafrate98@gmail.com>
2022-09-05 12:06:30 +0200

    Changes to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='my_environment',
     version='0.0.2',
     install_requires=['gym==0.21.0', 'scipy==1.7.3','pygame==2.1.2', 'numpy==1.21.*', 'wandb','pyvista'],
-    include_package_data=True,
-    package_dir={"": "my_environment"}
+    #include_package_data=True,
+    #package_dir={"": "my_environment"}
     )

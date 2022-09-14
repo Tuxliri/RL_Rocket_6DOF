7ed02142a07290f3732f79aac29b3f290aaa830e
Tuxliri <dvde.iafrate98@gmail.com>
2022-09-14 16:44:17 +0200

    Succesful landing with previously trained model
    *The model previously trained on the simplified IC range has been tested
    
    *On the expanded set of IC the model appears to still achieve landing

--- test_6DOF_environment.py
+++ test_6DOF_environment.py
@@ -23,31 +23,31 @@ with open("config.yaml") as f:
     env_config = config["env_config"]
 
 # Instantiate the environment
-kwargs = env_config
-env = Rocket6DOF(**kwargs)
+env = Rocket6DOF(**env_config)
 
 # env=RecordVideo(env,video_folder="video_6DOF")
 
 # [delta_y, delta_z, thrust]
 null_action = [0.0, 0.0, -1]
 non_null_action = [1.0, 1.0, -0.5]
-
+model = PPO.load('model_brisk-donkey-8.zip')
 
 # Initialize the environment
 done = False
 obs = env.reset()
 env.render(mode="human")
 
-while True:
-    obs, rew, done, info = env.step(null_action)
+while not done:
+    action, __, = model.predict(obs)
+    obs, rew, done, info = env.step(action)
     env.render(mode="human")
     if done:
+        fig = env.get_vtarg_plotly()
         env.reset()
         env.render(mode="human")
 
 vtarg_dataframe = env.vtarg_to_dataframe()
-# fig = env.get_trajectory_plotly()
-fig = vtarg_dataframe.plot()
-env.close()
 
 fig.show()
+
+env.close()

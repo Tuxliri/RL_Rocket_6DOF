cd76a9745f811906cf47290f834ef42f0b9ff34b
Tuxliri <dvde.iafrate98@gmail.com>
2022-09-28 11:42:27 +0200

    No landing reward test

--- my_environment/envs/rocket_env.py
+++ my_environment/envs/rocket_env.py
@@ -374,8 +374,8 @@ class Rocket6DOF(Env):
             map(self.reward_coefficients.get,["kappa","w_r_f", "w_v_f","max_r_f", "max_v_f"])
             )
         
-        if landing_conditions["zero_height"] and landing_conditions["landing_radius"]:
-            final_rewards = np.maximum([max_r_f-r,max_v_f-v],0) * [w_r_f, w_v_f]
+        #if landing_conditions["zero_height"] and landing_conditions["landing_radius"]:
+        #    final_rewards = np.maximum([max_r_f-r,max_v_f-v],0) * [w_r_f, w_v_f]
 
         return {
             'goal_conditions': k*all(landing_conditions.values()),

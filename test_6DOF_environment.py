c19f34f73e90e7bc340db25475488a9d9dceb6b0
Tuxliri <dvde.iafrate98@gmail.com>
2022-12-03 11:45:28 +0100

    Improved environment visualization

--- test_6DOF_environment.py
+++ test_6DOF_environment.py
@@ -2,12 +2,9 @@
 Script to test functionality of the 6DOF environment
 """
 
-from genericpath import isfile
 from my_environment.envs import Rocket6DOF
-from gym.wrappers import RecordVideo
 
 # Import the initial conditions from the setup file
-from gym.wrappers import RecordVideo
 
 import pandas as pd
 import os
@@ -17,10 +14,11 @@ pd.options.plotting.backend = "plotly"
 from stable_baselines3.ppo.ppo import PPO
 
 from main_6DOF import load_config
+from wrappers import RemoveMassFromObs
 
 sb3_config, env_config, = load_config()
 # Instantiate the environment
-env = Rocket6DOF(**env_config)
+env = RemoveMassFromObs(Rocket6DOF(**env_config))
 
 # env=RecordVideo(env,video_folder="video_6DOF")
 
@@ -32,8 +30,9 @@ def get_action(action_type = 'null'):
     elif action_type == 'constant':
         return [1.0, 1.0, -0.5]
         
-    elif os.path.isfile('model_brisk-donkey-8.zip'):
-        model = PPO.load('model_brisk-donkey-8.zip')
+    elif action_type == 'model':
+        assert os.path.isfile('current_model.zip'), 'Model file doesn\'t exist'
+        model = PPO.load('current_model.zip')
         action, __, = model.predict(obs)
         return action
 
@@ -47,18 +46,18 @@ landing_attempts = 1
 succesful_landings = 0
 
 while landing_attempts <= 10:
-    action = get_action()
+    action = get_action('model')
     obs, rew, done, info = env.step(action)
     env.render(mode="human")
     
     if done:
-        landing_attempts += 1
-        if info['is_succesful'] is True:
-            succesful_landings +=1
-        print(info["landing_conditions"])
+    #     landing_attempts += 1
+    #     if info['is_succesful'] is True:
+            # succesful_landings +=1
+        # print(info["landing_conditions"])
         env.reset()
         env.render(mode="human")
 
-print(f"The success rate is:{succesful_landings/landing_attempts*100}%")
+# print(f"The success rate is:{succesful_landings/landing_attempts*100}%")
 
 env.close()

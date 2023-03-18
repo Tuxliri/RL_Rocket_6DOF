b51ae64087527691b06eaf9cebb7fab7a389aa42
davide <Tuxliri@users.noreply.github.com>
2023-03-18 10:20:53 +0000

    Fixed __init__.py files

--- my_environment/__init__.py
+++ my_environment/__init__.py
@@ -1,7 +1,9 @@
 from gym.envs.registration import register
 
+from .envs import Rocket6DOF, Rocket6DOF_Fins
+
 # Register the environment
 register(
-    id='my_environment/Falcon6DOF-v0',
-    entry_point='my_environment.envs:Rocket6DOF'
+    id='my_environment/Falcon6DOF-v1',
+    entry_point='my_environment.envs:Rocket6DOF_Fins'
 )
--- my_environment/envs/__init__.py
+++ my_environment/envs/__init__.py
@@ -1,2 +1,4 @@
 from my_environment.envs.rocket_env import *
+from my_environment.envs.rocket_env_fins import Rocket6DOF_Fins
+
 from my_environment.utils import simulator

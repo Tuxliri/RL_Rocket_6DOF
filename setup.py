cddf90a3e5a44e174935b20bdd43a3fca0b19140
davide <Tuxliri@users.noreply.github.com>
2023-06-17 18:49:40 -0500

    Cleaned up files

--- setup.py
+++ setup.py
@@ -4,6 +4,4 @@ setup(
     name='my_environment',
     version='0.0.2',
     install_requires=['gym==0.21.0', 'scipy==1.7.3','pygame==2.1.2', 'numpy==1.21.*', 'wandb','pyvista','stable_baselines3'],
-    #include_package_data=True,
-    #package_dir={"": "my_environment"}
     )

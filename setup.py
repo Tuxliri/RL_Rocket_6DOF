e08b6d0f5e343b7df3c45e633aa91326f8a4d0cb
davide <Tuxliri@users.noreply.github.com>
2023-06-17 22:11:53 -0500

    Update simulator version

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
     name='my_environment',
-    version='0.0.2',
-    install_requires=['gym==0.21.0', 'scipy==1.7.3','pygame==2.1.2', 'numpy==1.21.*', 'wandb','pyvista','stable_baselines3'],
+    version='0.0.3',
+    install_requires=['gym==0.21.0', 'scipy==1.7.3','pygame==2.1.2' , 'wandb','pyvista',],
     )
